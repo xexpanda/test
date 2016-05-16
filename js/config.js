77a27c2d88c127870283b3034226b9eb1adaa7ea
xexpanda <xexpandav66666666@gmail.com>
2016-05-15 21:53:28 -0700

    1

---
+++ js/config.js
@@ -0,0 +1,111 @@
+
+/*URL for the server logo
+Leave it blank to use text instead*/
+var l_serverImage = "images/logo.png";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "Trunks";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = true;
+
+/*Video background
+
+You can add custom backgrounds
+Just make sure it is at webm VP8 format */
+var l_background = "backgrounds/videos/1.webm";
+var l_background = "backgrounds/videos/2.webm";
+var l_background = "backgrounds/videos/3.webm";
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them*/
+var l_bgImages = [
+	
+	
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = true;
+
+/*Display actual song's name?*/
+var l_musicDisplay = true;
+
+/*Music playlist
+Place how much Youtube IDs/.ogg you want*/
+var l_musicPlaylist = [
+	{ogg: "songs/NewCharlotte.ogg", name: "DuRu Tha King - New Charlotte"},
+	{ogg: "songs/MikeStudTheseDays.ogg", name: "Mike Stud - These Days"},
+	{ogg: "songs/AllTimeLow.ogg", name: "Jon Bellion - All Time Low"},
+    {ogg: "songs/DarkLight.ogg", name: "Night Lovell - Dark Light"},
+	{ogg: "songs/DeadOrAlive.ogg", name: "Jazz Cartier - Dead Or Alive"},
+	{ogg: "songs/Get Buck.ogg", name: "JPouya - Get Buck | PROD. BY Rellim"},
+	{ogg: "songs/Like Me.ogg", name: "Logic Ft. Casey Veggies - Like Me"},
+	{ogg: "songs/LucyOlivve_.ogg", name: "Skizzy Mars - Lucy (Olivver Remix)"},
+	{ogg: "songs/Monica Lewinsky.ogg", name: "G-Eazy ft. Skizzy Mars & KYLE - Monica Lewinsky"},
+	{ogg: "songs/Panda.ogg", name: "Desiigner - Panda"},
+	{ogg: "songs/tea.ogg", name: "XXXTENTACION Ft. XANNER- BITCH IM SIPPIN TEA IN YO HOOD"},
+	{ogg: "songs/TreesOfTheValley.ogg", name: "Night Lovell - Trees Of The Valley"},
+	{ogg: "songs/Ultimate.ogg", name: "Denzel Curry-  Ultimate"},
+	{ogg: "songs/Wood.ogg", name: "Jon Bellion - Woodstock (Psychedelic Fiction)"},
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 25;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+"Enjoy the music",
+"Thanks for joining us!",
+];
+
+/*Random message order?*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
